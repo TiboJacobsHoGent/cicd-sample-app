ddc05b0b13a3dd1dedddb343d5272887adc46d00
TiboJacobsHoGent <47555337+TiboJacobsHoGent@users.noreply.github.com>
2021-10-18 13:49:52 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -5,11 +5,11 @@ mkdir tempdirr
 mkdir tempdirr/templates
 mkdir tempdirr/static
 
-cp sample_app.py tempdir/.
-cp -r templates/* tempdir/templates/.
-cp -r static/* tempdir/static/.
+cp sample_app.py tempdirr/.
+cp -r templates/* tempdirr/templates/.
+cp -r static/* tempdirr/static/.
 
-cat > tempdir/Dockerfile << _EOF_
+cat > tempdirr/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
@@ -19,7 +19,7 @@ EXPOSE 5050
 CMD python /home/myapp/sample_app.py
 _EOF_
 
-cd tempdir || exit
+cd tempdirr || exit
 docker build -t sampleapp .
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a 
